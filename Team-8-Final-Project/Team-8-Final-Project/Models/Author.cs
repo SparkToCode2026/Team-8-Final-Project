a2d004f95091ed936f479b3dfeb1ade634ddd0d3
AbdulraheemHilal <gtp02684@gmail.com>
2026-08-04 22:42:35 +0400

    Add User and Author initial models

---
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Team_8_Final_Project.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Bio { get; set; }
+    }
+}
---
+++ Team-8-Final-Project/Team-8-Final-Project/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Team_8_Final_Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "Member"; // Member, Librarian, Admin
+    }
+}

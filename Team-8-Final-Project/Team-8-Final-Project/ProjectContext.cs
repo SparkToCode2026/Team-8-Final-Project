5b5d0ed3fc685de8d99b6768a3db7c40313e29e2
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-08-04 11:50:44 +0400

    Added registering models section and connecting to database section in ProjectContext.cs

--- Team-8-Final-Project/Team-8-Final-Project/ProjectContext.cs
+++ Team-8-Final-Project/Team-8-Final-Project/ProjectContext.cs
@@ -1,7 +1,22 @@
-﻿namespace Team_8_Final_Project
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Team_8_Final_Project
 {
     public class ProjectContext
     {
+        // 1 - register models
 
+        // public DbSet<Employee> employees { get; set; } // change name to model name
+        // public DbSet<Department> departments { get; set; } // change name to model name
+
+
+        // 2 - connect to database
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+            "Server=;Database=CompanyProjectDB;Trusted_Connection=True;TrustServerCertificate=True;" // change server name to your server name when testing 
+            );
+        }
     }
 }

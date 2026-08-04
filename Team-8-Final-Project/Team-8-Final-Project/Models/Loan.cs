b2436c1c928b150e62fd45165166e555e6dfb63b
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-08-04 15:49:42 +0400

    added loan attributes to loan class entity: LoanId, BookCopyId, UserId, LoanStartDate, LoanEndDate, LoanReturnDate

--- Team-8-Final-Project/Team-8-Final-Project/Models/Loan.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Loan.cs
@@ -2,5 +2,12 @@
 {
     public class Loan
     {
+        public int LoanId { get; set; }
+        public int BookCopyId { get; set; }
+        public int UserId { get; set; }
+        public string LoanStartDate { get; set; }
+        public string LoanEndDate { get; set; }
+        public string LoanReturnDate { get; set; }
+
     }
 }

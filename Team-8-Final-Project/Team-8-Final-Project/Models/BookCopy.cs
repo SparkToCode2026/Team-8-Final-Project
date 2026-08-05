902efc13a37a8f1c334d1961822b9032ab6c1202
Aya Salama <ayasl2905@gmail.com>
2026-08-05 22:22:00 +0400

    Implemented the BookCopy Model

---
+++ Team-8-Final-Project/Team-8-Final-Project/Models/BookCopy.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Team_8_Final_Project.Models
+{
+    public enum ConditionStatus
+    {
+        New,
+        Good,
+        Fair,
+        Poor
+    }
+
+    public enum AvailabilityStatus
+    {
+        Available,
+        OnLoan,
+        Reserved
+    }
+
+    public class BookCopy
+    {
+        [Key]
+        public int BookCopyId { get; set; }
+        public string Barcode { get; set; }
+        public ConditionStatus Condition { get; set; }
+        public AvailabilityStatus AvailabilityStatus { get; set; }
+
+    }
+}

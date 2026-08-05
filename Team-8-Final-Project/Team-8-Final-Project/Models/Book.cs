a033b577e7d6aa902906d6bcc1b11efe36b495e0
Aya Salama <ayasl2905@gmail.com>
2026-08-05 22:09:36 +0400

    Implemented the Book Model

--- Team-8-Final-Project/Team-8-Final-Project/Models/Book.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Book.cs
@@ -1,7 +1,21 @@
-﻿namespace Team_8_Final_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Team_8_Final_Project.Models
 {
     public class Book
     {
+        [Key]
+        [JsonIgnore]
+        public int BookId {  get; set; }
+        public string IBSN { get; set; }
+        public string BookTitle { get; set; }
+        public int BookEdition { get; set; }
+        public string BookLanguage { get; set; }
+        public int Year { get; set; }
+
+
+
 
     }
 }

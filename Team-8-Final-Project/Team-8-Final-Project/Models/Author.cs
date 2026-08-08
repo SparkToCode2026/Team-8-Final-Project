68b81e1aeaa3450b9d7a364ec560b4927f72fd32
Aya Salama <ayasl2905@gmail.com>
2026-08-08 12:57:43 +0300

    Added [Key] and [JsonIgnore] in Author

--- Team-8-Final-Project/Team-8-Final-Project/Models/Author.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Author.cs
@@ -1,7 +1,12 @@
-﻿namespace Team_8_Final_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Team_8_Final_Project.Models
 {
     public class Author
     {
+        [Key]
+        [JsonIgnore]
         public int AuthorID { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;

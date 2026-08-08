30380193faa064a8b03d8282a196d37a47598727
Aya Salama <ayasl2905@gmail.com>
2026-08-08 18:24:28 +0300

    Fixed the author class

--- Team-8-Final-Project/Team-8-Final-Project/Models/Author.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Author.cs
@@ -8,10 +8,10 @@ namespace Team_8_Final_Project.Models
         [Key]
         [JsonIgnore]
         public int AuthorID { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string? Biography { get; set; }
-        public string? Nationality { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Biography { get; set; }
+        public string Nationality { get; set; }
     }
 }

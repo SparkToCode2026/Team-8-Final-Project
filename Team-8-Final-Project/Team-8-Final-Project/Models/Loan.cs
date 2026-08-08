3efc317b19486d3a93132e5dc01ac3561415d0f0
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-08-08 11:21:20 +0400

    added loan status and foreign keys

--- Team-8-Final-Project/Team-8-Final-Project/Models/Loan.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Loan.cs
@@ -1,13 +1,27 @@
 ﻿namespace Team_8_Final_Project.Models
 {
+    public enum LoanStatus
+    {
+        Active,
+        Returned,
+        Overdue
+    }
+
     public class Loan
     {
         public int LoanId { get; set; }
-        public int BookCopyId { get; set; }
-        public int UserId { get; set; }
         public string LoanStartDate { get; set; }
         public string LoanEndDate { get; set; }
         public string LoanReturnDate { get; set; }
+        public string LoanStatus { get; set; }
+
+
+        [ForeignKey("BookCopyId")]
+        public int BookCopyId { get; set; }
+        public BookCopy BookCopy { get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
 
     }
 }

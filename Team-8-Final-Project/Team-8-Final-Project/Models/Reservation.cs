4726fe9b77844366f36414fd3d88ed4364b6ff64
Aya Salama <ayasl2905@gmail.com>
2026-08-08 19:07:07 +0300

    fixed enum in reservation

--- Team-8-Final-Project/Team-8-Final-Project/Models/Reservation.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Reservation.cs
@@ -16,14 +16,14 @@ namespace Team_8_Final_Project.Models
         [JsonIgnore]
         public int ReservationId { get; set; }
         public string ReservationDate { get; set; }
-        public string ReservationStatus { get; set; }
+        public ReservationStatus status { get; set; }
 
         [ForeignKey("bookCopy")]
         public int BookCopyId { get; set; }
         public BookCopy bookCopy { get; set; }
 
         [ForeignKey("user")]
-        public int UserID { get; set; }
+        public int UserId { get; set; }
         public User user { get; set; }
     }
 }

618af27e12f088a47fa7ef4535042a7139dd76c4
Aya Salama <ayasl2905@gmail.com>
2026-08-09 01:04:53 +0300

    Fixes in Shelf class

--- Team-8-Final-Project/Team-8-Final-Project/Models/Shelf.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Shelf.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace Team_8_Final_Project.Models;
 
 public class Shelf
 {
+    [Key]
+    [JsonIgnore]
     public int ShelfID { get; set; }
     public string ShelfCode { get; set; }
     public string Section { get; set; }
     public int FloorNumber { get; set; }
-}
+
 
 }

8c1972ad5b0928805eb8b03b821e8169d44b53c4
Aya Salama <ayasl2905@gmail.com>
2026-08-08 13:01:36 +0300

    Added [Key] and JsonIgnore in Reservation

--- Team-8-Final-Project/Team-8-Final-Project/Models/Reservation.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Reservation.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Team_8_Final_Project.Models
 {
@@ -10,6 +12,8 @@ namespace Team_8_Final_Project.Models
     }
     public class Reservation
     {
+        [Key]
+        [JsonIgnore]
         public int ReservationId { get; set; }
         public string ReservationDate { get; set; }
         public string ReservationStatus { get; set; }

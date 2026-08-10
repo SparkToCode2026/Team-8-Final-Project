c50a7c2ca6b87a4f3818f08fc99bc353ea9717c5
Aya Salama <ayasl2905@gmail.com>
2026-08-10 03:27:21 +0300

    Fixed the loan.cs class

--- Team-8-Final-Project/Team-8-Final-Project/Models/Loan.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Loan.cs
@@ -6,46 +6,34 @@ using System.Text.Json.Serialization;
 
 namespace Team_8_Final_Project.Models
 {
+    public enum LoanStatus
+    {
+        Active,
+        Overdue,
+        Returned
+    }
     public class Loan
     {
         [Key]
         [JsonIgnore]
-        public int LoanID { get; set; }
+        public int LoanId { get; set; }
 
         [Required]
         public DateTime LoanStartDate { get; set; }
 
         [Required]
         public DateTime LoanDueDate { get; set; }
-
-        public DateTime? LoanReturnDate { get; set; }
-
-        [Required]
-        [StringLength(20)]
-        public string LoanStatus { get; set; } = string.Empty;
-        public int LoanId { get; set; }
-        public string LoanStartDate { get; set; }
-        public string LoanEndDate { get; set; }
-        public string LoanReturnDate { get; set; }
-        public LoanStatus  loanStatus { get; set; }  
+        public DateTime LoanReturnDate { get; set; }
+        public LoanStatus  loanStatus { get; set; }
 
         // Foreign Key - BookCopy
-        [Required]
-        public int BookCopyID { get; set; }
-
-        [ForeignKey(nameof(BookCopyID))]
-        public BookCopy? BookCopy { get; set; }
-
-        // Foreign Key - User
-        [Required]
         [ForeignKey("bookCopy")]
         public int BookCopyId { get; set; }
         public BookCopy bookCopy { get; set; }
 
+        // Foreign Key - User
         [ForeignKey("user")]
         public int UserID { get; set; }
-
-        [ForeignKey(nameof(UserID))]
-        public User? User { get; set; }
+        public User user { get; set; }
     }
 }

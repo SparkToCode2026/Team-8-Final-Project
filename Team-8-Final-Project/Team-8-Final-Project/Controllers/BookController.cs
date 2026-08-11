8f1864f5731a1b568130cdbc3b2a597bb7b4b0d9
Aya Salama <ayasl2905@gmail.com>
2026-08-11 14:26:27 +0300

    Added UpdateBook Function

--- Team-8-Final-Project/Team-8-Final-Project/Controllers/BookController.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Controllers/BookController.cs
@@ -22,6 +22,33 @@ namespace Team_8_Final_Project.Controllers
             return Ok(b);
         }
 
+        // Full Update of a Book
+        [HttpPut("UpdateBook/{id}")]
+        public IActionResult UpdateBook(int id, Book newBook)
+        {
+            Book b = context.books.FirstOrDefault(b => b.BookId == id);
+
+            if (b == null)
+            {
+                return NotFound("Book not found.");
+            }
+
+            b.BookTitle = newBook.BookTitle;
+            b.ISBN = newBook.ISBN;
+            b.Year = newBook.Year;
+            
+            b.BookLanguage = newBook.BookLanguage;
+            b.BookEdition = newBook.BookEdition;
+            b.Authors = newBook.Authors;
+
+            b.PublisherId = newBook.PublisherId;
+            b.CategoryId = newBook.CategoryId;
+
+            context.SaveChanges();
+
+            return Ok(newBook);
+        }
+
     }
-    
+
 }

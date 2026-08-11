c0a320f8a61e70e22327980abdd41db7fcefcbb9
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-08-11 20:00:02 +0400

    added public class loancontroller and loancontroller context

--- Team-8-Final-Project/Team-8-Final-Project/Controllers/LoanController.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Controllers/LoanController.cs
@@ -1,7 +1,17 @@
-﻿namespace Team_8_Final_Project.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_8_Final_Project.Controllers
 {
     public class LoanController
     {
-        
+        public class LoanController : ControllerBase
+        {
+            private ProjectContext context;
+
+            public LoanController(ProjectContext _context) 
+            {
+                context = _context;
+            }
+        }
     }
 }

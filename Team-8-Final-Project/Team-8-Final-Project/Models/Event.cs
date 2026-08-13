f468714a175a22568f134cda640d1740aa3cc899
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-08-13 23:49:57 +0400

    added eventstatus in event.cs

--- Team-8-Final-Project/Team-8-Final-Project/Models/Event.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Models/Event.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Team_8_Final_Project.Models;
+
+public enum EventStatus
+{
+    Upcoming,
+    Ongoing,
+    Completed,
+    Cancelled
+}
 
 public class Event
 {
@@ -11,13 +21,14 @@ public class Event
     [Required]
     public string EventName { get; set; }
     [Required]
-    public DateTime EventDate{ get; set; }
+    public DateTime EventDate { get; set; }
     [Required]
     public string EventLocation { get; set; }
     public string EventDescription { get; set; }
     public int EventMaxCap { get; set; }
-    
-    
+    public EventStatus Status { get; set; } = EventStatus.Upcoming;
+
+
 
     public List<User> Users { get; set; }
 }

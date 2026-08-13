81fca2195557695a21f76e257f37442bd6752b0b
Aya Salama <ayasl2905@gmail.com>
2026-08-13 13:38:32 +0300

    Created the Defaukt i the controller

--- Team-8-Final-Project/Team-8-Final-Project/Controllers/AuthController.cs
+++ Team-8-Final-Project/Team-8-Final-Project/Controllers/AuthController.cs
@@ -1,6 +1,21 @@
-﻿namespace Team_8_Final_Project.Controllers
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Team_8_Final_Project.Models;
+namespace Team_8_Final_Project.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("AuthController")]
+    public class AuthController : ControllerBase
     {
+        private ProjectContext context;
+
+        public AuthController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+       
     }
+
+
 }

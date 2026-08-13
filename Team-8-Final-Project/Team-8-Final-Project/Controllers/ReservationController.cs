15713f7c4e5f3a94fbbdac7e6ca5ebd075f257db
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-08-13 13:01:19 +0400

    Updated ReservationController

---
+++ Team-8-Final-Project/Team-8-Final-Project/Controllers/ReservationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team_8_Final_Project.Controllers
+{
+    [ApiController]
+    [Route("Reservation")]
+    public class ReservationController : ControllerBase
+    {
+
+
+        private ProjectContext context;
+
+        public ReservationController(ProjectContext _context)
+        {
+            context = _context;
+        }
+    }
+
+    public class CreateReservationDto
+    {
+        [Required]
+        public DateTime ReservationDate { get; set; }
+
+        [Required]
+        public string ReservationStatus { get; set; }
+
+    }
+
+}

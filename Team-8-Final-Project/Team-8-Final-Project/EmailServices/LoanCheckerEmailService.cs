9a415ea0daacdeae262c0fdf2a615af442d07d58
Aya Salama <ayasl2905@gmail.com>
2026-08-15 02:02:21 +0300

    created the LoanCheckerEmailService file and added all the services needed in it

---
+++ Team-8-Final-Project/Team-8-Final-Project/EmailServices/LoanCheckerEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Team_8_Final_Project.Models;
+
+namespace Team_8_Final_Project.EmailServices
+{
+    public class LoanCheckerEmailService : BackgroundService
+    {
+        private readonly IServiceScopeFactory scopeFactory;
+        private readonly EmailService emailService;
+    }
+}

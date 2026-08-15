19756e6894757052ee3b5d1fe41de8153a14dfae
Aya Salama <ayasl2905@gmail.com>
2026-08-15 04:56:53 +0300

    loan reminder  email logic

--- Team-8-Final-Project/Team-8-Final-Project/EmailServices/LoanCheckerEmailService.cs
+++ Team-8-Final-Project/Team-8-Final-Project/EmailServices/LoanCheckerEmailService.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Team_8_Final_Project.Models;
 
 namespace Team_8_Final_Project.EmailServices
@@ -25,6 +26,35 @@ namespace Team_8_Final_Project.EmailServices
                 using (IServiceScope scope = scopeFactory.CreateScope())
                 {
                     ProjectContext context = scope.ServiceProvider.GetRequiredService<ProjectContext>();
+
+                    List<Loan> activeLoans = context.Loans.Include(l => l.User)
+                                                          .Where(l => l.loanStatus == LoanStatus.Active)
+                                                          .ToList();
+
+                    foreach (Loan loan in activeLoans)
+                    {
+                        TimeSpan timeUntilDue = loan.LoanDueDate - DateTime.Now;
+
+                        if (loan.LoanDueDate < DateTime.Now)
+                        {
+                            loan.loanStatus = LoanStatus.Overdue;
+                        }
+                        else if (loan.LoanDueDate.Date == DateTime.Now.Date.AddDays(1))
+                        {
+                             string body =
+                             "Hello " + loan.User.FirstName + ",\n\n" +
+                             "This is a reminder that your borrowed book is due tomorrow.\n\n" +
+                             "Due date: " + loan.LoanDueDate.ToShortDateString() + "\n\n" +
+                             "Please return the book on time to avoid an overdue fine.\n\n" +
+                             "Library Management System";
+
+                            await emailService.SendEmailAsync( loan.User.UserEmail,
+                             "Library Loan Reminder",
+                               body );
+                        }
+
+                    }
+
                 }
 
                 await Task.Delay( TimeSpan.FromMinutes(intervalMinutes), stoppingToken );
